9ef8102e0dbec72782b85888f700ca277883ccfe
Gabriel <gabriel.romano.3194@hotmail.com>
2021-02-27 16:40:19 -0300

    Implementando o delete

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,7 +75,14 @@ public class TopicosController {
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
 		Topico topico = form.atualizar(id, topicoRepository);
 		TopicoDto dto = new TopicoDto(topico);
-		return ResponseEntity.ok(dto); //o ok retorna 200
+		return ResponseEntity.ok(dto); //o ok retorna 200 com o dto no body da response
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional 
+	public ResponseEntity<?> deletar(@PathVariable Long id) {
+		topicoRepository.deleteById(id);
+		return ResponseEntity.ok().build(); //retorna 200 sem nada no body
 	}
 	
 }
