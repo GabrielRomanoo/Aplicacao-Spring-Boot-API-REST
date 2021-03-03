a86b732ee8bc55447f2f3cee8d146b79f7551fc2
Gabriel <gabriel.romano.3194@hotmail.com>
2021-03-03 15:48:45 -0300

    autenticacao service

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$9wTg8ZeL6zHERi15n9gomezpbYfYTx8PssyF/qgrE3KUHVK8Semjq');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
