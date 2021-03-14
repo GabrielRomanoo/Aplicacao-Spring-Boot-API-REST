3ded4fdfbf3a8f929ac149248762a639b2598972
Gabriel <gabriel.romano.3194@hotmail.com>
2021-03-14 15:21:46 -0300

    testando repository

---
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,33 @@
+package br.com.alura.forum.repository;
+
+
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.alura.forum.modelo.Curso;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CursoRepositoryTest {
+
+	@Autowired
+	private CursoRepository repository;
+	
+	@org.junit.Test
+	public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
+		String nomeCurso = "HTML 5";
+		Curso curso = repository.findByNome(nomeCurso);
+		Assert.assertNotNull(curso);
+		Assert.assertEquals("HTML 5", curso.getNome());
+	}
+
+	@org.junit.Test
+	public void naoDeveriaCarregarUmCursoCujoNomeNaoEstejaCadastrado() {
+		String nomeCurso = "JPA";
+		Curso curso = repository.findByNome(nomeCurso);
+		Assert.assertNull(curso);
+	}
+}
